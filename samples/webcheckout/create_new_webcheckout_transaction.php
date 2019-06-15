c33a868bf326e2c6e0aa0075802c90f8577f6dfd
R.Pani <rpani@my.cl>
2019-06-15 02:14:35 -0400

    updated webcheckout sample

--- samples/webcheckout/create_new_webcheckout_transaction.php
+++ samples/webcheckout/create_new_webcheckout_transaction.php
@@ -1,13 +1,48 @@
 <?php
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+
 use \Paygol\Webcheckout;
 use \Paygol\Models\Payer;
 use \Paygol\Models\RedirectUrls;
 
 use \Paygol\Exceptions\InvalidParameterException;
 
+$products = [
+    1234 => [
+        'name' => 'Basic',
+        'price' => 99.99,
+        'currency' => 'USD'
+    ],
+    1235 => [
+        'name' => 'Standard',
+        'price' => 199.99,
+        'currency' => 'USD'
+    ],
+    1236 => [
+        'name' => 'Unlimited',
+        'price' => 299.99,
+        'currency' => 'USD'
+    ],
+];
 
+$product_id = $_GET['productid'];
+
+$product = $products[$product_id];
+
+
+/**
+ * Merchant service ID
+ *
+ * @var int
+ */
 $service_id = "123";
+/**
+ * Merchant shared secret
+ *
+ * @var string
+ */
 $shared_secret = "7c1a6a24-7943-102d-92f8-29573711ad31";
 
 try {
@@ -23,7 +58,9 @@ try {
 
     $pg->setCountry('DE');
 
-    $pg->setPrice(100, 'EUR');
+    $pg->setPrice($product['price'], $product['currency']);
+
+    $pg->setName($product['name']);
 
     $payer = new Payer();
     $payer->setEmail('jdoe@my-site.com');
